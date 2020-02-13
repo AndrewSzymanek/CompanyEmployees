2426106b86ef55c572c8971b165992d0e3f58642
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2020-02-13 09:16:43 -0600

    moved configureservices() method to the startup class instead of servicextensions to make sure logging messages work

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -13,13 +13,7 @@ namespace CompanyEmployees.Extensions
     {
         public static void ConfigureLoggerService(this IServiceCollection services) => services.AddScoped<ILoggerManager, LoggerManager>();
 
-        public static void ConfigureServices(IServiceCollection services)
-        {
-            services.ConfigureCors();
-            services.ConfigureIISIntegration();
-            services.ConfigureLoggerService();
-            services.AddControllers();
-        }
+
         public static void ConfigureCors(this IServiceCollection services) =>
             services.AddCors(options =>
             {
