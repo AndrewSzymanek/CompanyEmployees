905afcb984b220ddb62eb576ba4d95a1e8bb5dd1
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2020-02-14 12:42:56 -0600

    added entities project to solution, referenced main project, created company and employees models

--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -29,12 +29,10 @@ namespace CompanyEmployees
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.ConfigureCors();
             services.ConfigureIISIntegration();
             services.ConfigureLoggerService();
             services.AddControllers();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
